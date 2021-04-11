5f38c3c29d59f030c29ed9c67f300608bab85a7a
crystal-g-b <crystal.barnett1@gmail.com>
2021-04-10 22:44:08 -0400

    updated Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,29 +12,57 @@ describe('Employee', () => {
     })
 
     it('it sets the name property based on constructor argument', () => {
-        const name = "Giselle",
+        const name = "Crystal",
         const employee = new Employee(name);
 
         expect(employee.name).toBe(name);
     })
-    it('it sets the name property based on constructor argument', () => {
+    it('it sets the id property based on constructor argument', () => {
         const id = 1,
         const employee = new Employee("", id);
 
         expect(employee.id).toBe(id);
     })
-    it('it sets the name property based on constructor argument', () => {
+    it('it sets the email property based on constructor argument', () => {
         const email = 'test@email.com',
         const employee = new Employee("", 0, email);
 
         expect(employee.email).toBe(email);
     });
-})
 
-describe ('getName', ( )=> {
 
-})
+    describe ('getName', ( )=> {
+        it("Can get name via getName()", () => {
+            const testValue = "Crystal";
+            const employee = new Employee(testValue);
 
-describe('getInfo', () => {
+            expect(employee.getName()).toBe(testValue);
+        });
+    });
 
-})
+    describe('getId', () => {
+        it("Can get id via getId()", () => {
+            const testValue = 1;
+            const employee = new Employee("", testValue);
+
+            expect(employee.getId()).toBe(testValue);
+        });
+    });
+
+    describe("getEmail", () => {
+        it("Can get email via getEmail()", () => {
+            const testValue = "crystal@js.com";
+            const employee = new Employee("", 1, testValue);
+
+            expect(employee.getEmail()).toBe(testValue);
+        });
+    });
+
+    describe("getRole", () => {
+        it("getRole() should return \"Employee\"", () => {
+            const testValue = "Employee";
+            const employee = new Employee("Crystal", 1, "crystal@js.com");
+            expect(employee.getRole()).toBe(testValue);
+        });
+    });
+});
